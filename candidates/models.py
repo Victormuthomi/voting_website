adb2818cd514061283b8a0ae5d5280b258379fbb
Victormuthomi <victormuhomi100@gmail.com>
2024-07-13 23:18:08 +0300

    Modify candidates/models/py.

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Candidiate(models.Model):
+    """Details of a candidate"""
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    id_number = models.IntegerField()
+    phone_number = models.IntegerField()
+    seat = models.char_field(max_length=20)
+

6fa785a369f9f54f50ef5eaf2ad3049a52f4d502
Victormuthomi <victormuhomi100@gmail.com>
2024-07-14 13:55:18 +0300

    Add the view for displaying the candidates page.

--- candidates/views.py
+++ candidates/views.py
@@ -5,3 +5,10 @@ def index(request):
     """The home page for Voting website"""
     return render(request, 'candidates/index.xhtml')
 
+def candidates(request):
+    """Show all the candidates"""
+    candidates = Candidate.objects.order_by('first_name')
+    context = { 'candidates' : candidates }
+
+    return render(request, 'candidates/candidates.xhtml', context)
+

6828f72480d3204120b16f8fa37ea578ecf3bd23
Victormuthomi <victormuhomi100@gmail.com>
2024-07-15 10:54:04 +0300

    Add a view to process the election page.

--- election_app/views.py
+++ election_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from . models import Election
 # Create your views here.
+def election(request):
+    """The page for returning the election"""
+    return render(request, 'election_app/election.xhtml')
+

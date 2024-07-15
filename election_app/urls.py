67eef2350d11185415f130bf7462665faf09971a
Victormuthomi <victormuhomi100@gmail.com>
2024-07-15 10:58:05 +0300

    Add urls for the election page.

---
+++ election_app/urls.py
@@ -0,0 +1,11 @@
+"""Define the urls for the election app"""
+
+from django.urls import path
+
+from .import views
+
+app_name = 'election_app'
+
+urlpatterns=[
+        path('election/', views.election, name='election')
+]

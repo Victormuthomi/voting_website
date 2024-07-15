7b7d88f5a15964eddf0e7b32a7b03087e6f69606
Victormuthomi <victormuhomi100@gmail.com>
2024-07-15 10:53:30 +0300

    Add the fields in the Election model.

--- election_app/models.py
+++ election_app/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Election(models.Model):
-    """
+    """A elction to be held"""
+    name = models.CharField(max_length=50)
+    date = models.DateField()
+    details = models.TextField()
+    

198a2694426fc60aa08341a26c9897e843f04e1d
Victormuthomi <victormuhomi100@gmail.com>
2024-07-28 23:08:20 +0300

    Modify views.py to render the results template.

--- results/views.py
+++ results/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def results(request):
+    """The page for returning the results"""
+    return render(request, 'results/results.xhtml')
+

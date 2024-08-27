2412e7367bd0a69e50f2e46192555095319c5b3a
Victormuthomi <victormuhomi100@gmail.com>
2024-08-27 13:42:30 +0300

    Add election field as an foreign key.

--- candidates/models.py
+++ candidates/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
+from election_app.models import Election 
+
 # Create your models here.
 class Candidate(models.Model):
     """Details of a candidate"""
+    election = models.ForeignKey(Election,on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     id_number = models.IntegerField()
     phone_number = models.IntegerField()
     seat = models.CharField(max_length=20)
     picture = models.ImageField()
-    manifesto = models.TestField()
-    motto = models.CharField(max_lengt=100)
+    manifesto = models.TextField()
+    motto = models.CharField(max_length=100)
     
 
     def __str__(self):

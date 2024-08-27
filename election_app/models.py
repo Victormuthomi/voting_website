8bf3bb6ea229a6d45c5a3eb171a93b7a61e90f8b
Victormuthomi <victormuhomi100@gmail.com>
2024-08-27 11:39:32 +0300

    Modify models.py.

--- election_app/models.py
+++ election_app/models.py
@@ -7,4 +7,7 @@ class Election(models.Model):
     name = models.CharField(max_length=50)
     date = models.DateField()
     details = models.TextField()
+    def __str__(self):
+        """Return the name of the election on the admin panel"""
+        return self.name
     

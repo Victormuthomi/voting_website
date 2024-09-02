01e51dc650096cc5ad6cd04058731a1887e02ab4
Victormuthomi <victormuhomi100@gmail.com>
2024-09-02 19:09:19 +0300

    Add a new model seat

--- candidates/models.py
+++ candidates/models.py
@@ -1,23 +1,24 @@
+# candidates/models.py
 from django.db import models
 
-from election_app.models import Election 
+class Seat(models.Model):
+    name = models.CharField(max_length=100)
+    # Add any other fields you need here
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
 class Candidate(models.Model):
-    """Details of a candidate"""
-    election = models.ForeignKey(Election,on_delete=models.CASCADE)
+    election = models.ForeignKey('election_app.Election', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     id_number = models.IntegerField()
     phone_number = models.IntegerField()
-    seat = models.CharField(max_length=20)
-    picture = models.ImageField()
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)  # Updated to ForeignKey
+    picture = models.ImageField(upload_to='candidate_pictures/')
     manifesto = models.TextField()
     motto = models.CharField(max_length=100)
-    
 
     def __str__(self):
-        """Return a string representation of the model"""
-        return self.first_name 
-
+        return self.first_name
 

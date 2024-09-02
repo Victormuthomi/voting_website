e7bbc2654153fa10a60dd91fee7d03068ad9c6d4
Victormuthomi <victormuhomi100@gmail.com>
2024-09-02 19:11:52 +0300

    Add the seat fiels.

--- voting/models.py
+++ voting/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from election_app.models import Election
-from candidates.models import Candidate 
+from candidates.models import Candidate
 
-# Create your models here.
-class vote(models.Model):
+class Vote(models.Model):
     """Details of the voting portal"""
-    
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='votes')
+    voting_value = models.IntegerField()  # Default value representing a vote, can be adjusted as needed
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        unique_together = ('candidate', 'election')
+
+    def __str__(self):
+        return f'Vote for {self.candidate} in {self.election}'
 
 

990272a88fa2b8866952f964fdc860dcd1e1738c
Victormuthomi <victormuhomi100@gmail.com>
2024-09-02 19:08:57 +0300

    Modify admin.py by adding a Seat model.

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Candidate
+from .models import Candidate, Seat
 
 # Register your models here.
 admin.site.register(Candidate)
+admin.site.register(Seat)
 
 
